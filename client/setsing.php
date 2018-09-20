a438b72626a06edff932a76dd69d739105143112
souding <sdouding@163.com>
2018-09-20 15:14:45 +0800

    使用时间控制api请求是否有效来解决api暴露问题

--- client/setsing.php
+++ client/setsing.php
@@ -2,11 +2,10 @@
 	include './aes.php';
 	//加密签名
 	function setsing($token){
+		$sing = $token.'/'.time();
 
 		//加密
-		$sing = AES::encrypt($token, 'g87y65ki6e8p93av8zjdrtfdrtgdwetd');
-		//解密
-		// $sing = AES::decrypt($token);
+		$sing = AES::encrypt($sing, 'g87y65ki6e8p93av8zjdrtfdrtgdwetd');
 		
 		return $sing;
 	}
--- server/checksing.php
+++ server/checksing.php
@@ -16,15 +16,22 @@
 	//校验签名
 	function check($sing){
 // ac637d50290092e0c0a530e55eb2668e
-
-
-		$decrypt = AES::decrypt($sing, 'g87y65ki6e8p93av8zjdrtfdrtgdwetd');
-		// echo $decrypt;
-		//解密
-	
 		
-		if($decrypt!=Token){
+		$sing = str_replace(' ','+', $sing);
+
+		//解密
+		$sing_tmp = AES::decrypt($sing, 'g87y65ki6e8p93av8zjdrtfdrtgdwetd');
+		// echo $decrypt;
+		$sing_arr = explode('/',$sing_tmp);
+		
+		if($sing_arr[0]!=Token){
 			throw new Exception("加密签名不正确");
+		}
+
+		//防止api暴露
+		if((time()-$sing_arr[1]) > 5){
+			throw new Exception("加密签名sing失效");
+			
 		}
 	}
 ?>
